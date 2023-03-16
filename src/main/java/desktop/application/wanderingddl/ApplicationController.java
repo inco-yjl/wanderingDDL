317a1ca39ff6f4b8d9d766ba15cb70f493a60981
wty <2479585756@qq.com>
2023-03-16 15:59:56 +0800

    ddl传参

--- src/main/java/desktop/application/wanderingddl/ApplicationController.java
+++ src/main/java/desktop/application/wanderingddl/ApplicationController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -19,28 +20,33 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
 import java.io.IOException;
-    public class ApplicationController extends Application {
-        static Stage stage;
-        static BorderPane window;
-        public Button wanderingPage;
+
+public class ApplicationController extends Application {
+    static Stage stage;
+    static BorderPane window;
+    public Button wanderingPage;
+
+    public Pane wanderingPageContent;
 
 
-        @Override
+    @Override
     public void start(Stage stage) throws IOException {
         ApplicationController.stage = stage;
         Parent root = FXMLLoader.load(getClass().getResource("Config/Window.fxml"));
         HBox windowMenu = (HBox) FXMLLoader.load(getClass().getResource("Config/Menubar.fxml"));
         VBox mainContent = (VBox) FXMLLoader.load(getClass().getResource("Config/Content.fxml"));
-        Pane  wanderingPageContent = (Pane) FXMLLoader.load(getClass().getResource("MainContent/WanderingPage.fxml"));
+        this.wanderingPageContent = (Pane) FXMLLoader.load(getClass().getResource("MainContent/WanderingPage.fxml"));
 
         Node[] pages = {wanderingPageContent};
 
-        createScene(root,windowMenu,mainContent);
+        createScene(root, windowMenu, mainContent);
         PageFactory.setPages(pages);
     }
+
     public void createScene(Parent root, HBox windowMenu, VBox mainContent) {
-        BorderPane window = (BorderPane)root;
+        BorderPane window = (BorderPane) root;
         ApplicationController.window = window;
         window.setTop(windowMenu);
         window.setRight(mainContent);
@@ -49,7 +55,7 @@ import java.io.IOException;
         stage.initStyle(StageStyle.TRANSPARENT);//隐藏头标题); //去除窗口样式
         scene.setFill(null);
         stage.setScene(scene);
-        DragUtil.addDragListener(stage,root); //窗口拖拽
+        DragUtil.addDragListener(stage, root); //窗口拖拽
         stage.show();
     }
 
@@ -57,29 +63,38 @@ import java.io.IOException;
     protected void closeWindow() {
         stage.close();
     }
+
     @FXML
     protected void minizeWindow() {
         stage.setIconified(true);
     }
+
     @FXML
-    protected void ToWanderingPage(){
+    protected void ToWanderingPage() {
         wanderingPage.setStyle("-fx-border-color: #a8ddb5;");
         routePage(0);
-        //此处直接打开了第一个
-        openWanderingUI();
+        //此处打开编辑文本框，不new新的
+        //openWanderingUI();
     }
-    protected void openWanderingUI(){
-            WanderingController wanderingController = new WanderingController();
+
+    @FXML
+    protected void openWanderingUI() {
+        String[] sentences = new String[4];
+        for (int i = 0; i < 4; i++)
+            sentences[i] = ((TextField) window.getRight().lookup("#" + Integer.toString(i))).getText();
+        WanderingController wanderingController = new WanderingController(sentences);
         try {
             wanderingController.start(new Stage());
-        }catch (IOException e){
+        } catch (IOException e) {
             //
         }
     }
+
     private void routePage(int index) {
-            Node node = PageFactory.createPageService(index);
-            window.setRight(node);
+        Node node = PageFactory.createPageService(index);
+        window.setRight(node);
     }
+
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/desktop/application/wanderingddl/WanderingController.java
+++ src/main/java/desktop/application/wanderingddl/WanderingController.java
@@ -32,44 +32,51 @@ public class WanderingController extends Application {
     private String[] strings;
     private Label[] texts = new Label[5]; //距离xxx,还有大概,30,space,天
 
-    public WanderingController(){
-        super();
+    public WanderingController(String[] sentences) {
+        //super();
         this.loadFont();
-        this.initText();
+        this.initText(sentences);
     }
-    private void loadFont(){
-        w_engFont= Font.loadFont(Objects.requireNonNull(getClass().getResource("Alte DIN 1451 Mittelschrift gepraegt Regular.ttf")).toExternalForm(), 84);
-        w_znFont=Font.loadFont(getClass().getResource("znFont.ttf").toExternalForm(), 36);
-        w_engTextFont = Font.loadFont(getClass().getResource("znFont.ttf").toExternalForm(),18);
+
+    private void loadFont() {
+        w_engFont = Font.loadFont(Objects.requireNonNull(getClass().getResource("Alte DIN 1451 Mittelschrift gepraegt Regular.ttf")).toExternalForm(), 84);
+        w_znFont = Font.loadFont(getClass().getResource("znFont.ttf").toExternalForm(), 36);
+        w_engTextFont = Font.loadFont(getClass().getResource("znFont.ttf").toExternalForm(), 18);
     }
+
     //传参初始化
-    private void initText(){
-        strings = new String[]{"距离某一科作业","还有","30","天","deadline comes in 5 days, wait for it patiently"};
-        for(int i=0;i<4;i++){
+    private void initText(String[] sentences) {
+        strings = new String[]{"距离某一科作业", "还有", "30", "天", "deadline comes in 5 days, wait for it patiently"};
+        for (int i = 0; i < 4; i++) {
             texts[i] = new Label(strings[i]);
+            if (sentences[i].length() > 0) {
+                texts[i] = new Label(sentences[i]);
+            }
         }
         texts[4] = new Label(" ");
-        setZnFont(new Label[]{texts[0],texts[1],texts[3],texts[4]});//text4是空的占位label
+        setZnFont(new Label[]{texts[0], texts[1], texts[3], texts[4]});//text4是空的占位label
         setNumFont(texts[2]);
     }
+
     public void start(Stage stage) throws IOException {
         //建立节点
         Pane all = new Pane();
         //载入字体，设置字体
-        all.getChildren().addAll(getHbox(),getEngText(),getSeparater());
+        all.getChildren().addAll(getHbox(), getEngText(), getSeparater());
         all.setStyle("-fx-background-color: transparent;");
 
-        Scene scene = new Scene(all, 480,262);
+        Scene scene = new Scene(all, 480, 262);
         scene.setFill(null);
         stage.initStyle(StageStyle.TRANSPARENT);//隐藏头标题); //去除窗口样式
         stage.setScene(scene);
-        stage.setX(Screen.getPrimary().getBounds().getWidth()-480);
-        stage.setY(Screen.getPrimary().getBounds().getHeight()-262);
-        DragUtil.addDragListener(stage,all);
+        stage.setX(Screen.getPrimary().getBounds().getWidth() - 480);
+        stage.setY(Screen.getPrimary().getBounds().getHeight() - 262);
+        DragUtil.addDragListener(stage, all);
         stage.show();
     }
+
     private HBox getHbox() {
-        HBox hbox= new HBox();
+        HBox hbox = new HBox();
         hbox.setAlignment(Pos.BOTTOM_RIGHT);
         hbox.getChildren().addAll(getLeft());
         hbox.getChildren().addAll(texts[2]);
@@ -77,76 +84,85 @@ public class WanderingController extends Application {
         hbox.setLayoutX(10);
         return hbox;
     }
-    private VBox getLeft(){
+
+    private VBox getLeft() {
         VBox left = new VBox();
         left.setAlignment(Pos.TOP_RIGHT);
-        left.getChildren().addAll(texts[0],texts[1]);
+        left.getChildren().addAll(texts[0], texts[1]);
         return left;
     }
-    private VBox getRight(){
+
+    private VBox getRight() {
         VBox right = new VBox();
         right.setAlignment(Pos.TOP_RIGHT);
-        right.getChildren().addAll(texts[4],texts[3]);
+        right.getChildren().addAll(texts[4], texts[3]);
         return right;
     }
-    private void setZnFont(Label[] labels){
-        for (Label label:
-             labels) {
+
+    private void setZnFont(Label[] labels) {
+        for (Label label :
+                labels) {
             label.setFont(w_znFont);
             label.setTextFill(Color.valueOf("#ffffff"));
         }
         setEffect(labels);
     }
+
     private void setNumFont(Label label) {
         label.setFont(w_engFont);
         label.setTextFill(Color.valueOf("#ff0000"));
         setEffect(label);
     }
-    private void setTextFont(Label label){
+
+    private void setTextFont(Label label) {
         label.setFont(w_engTextFont);
         label.setTextFill(Color.valueOf("#ffffff"));
         setEffect(label);
     }
-    private void setEffect(Label[] labels){
-        for (Label label:
+
+    private void setEffect(Label[] labels) {
+        for (Label label :
                 labels) {
             setEffect(label);
         }
     }
-    private void setEffect(Label label){
-            DropShadow ds = new DropShadow();
-            ds.setOffsetY(0f);
-            ds.setColor(Color.valueOf("#333333"));
-            label.setEffect(ds);
+
+    private void setEffect(Label label) {
+        DropShadow ds = new DropShadow();
+        ds.setOffsetY(0f);
+        ds.setColor(Color.valueOf("#333333"));
+        label.setEffect(ds);
     }
-    private Rectangle getSeparater(){
+
+    private Rectangle getSeparater() {
         Rectangle rectangle = new Rectangle();
-        int evH = strings[4].length()*18*20/(strings[1].length()*36+strings[3].length()*36+strings[2].length()*84);
+        int evH = strings[4].length() * 18 * 20 / (strings[1].length() * 36 + strings[3].length() * 36 + strings[2].length() * 84);
         //计算理想高度
-        rectangle.setHeight(evH+40);
+        rectangle.setHeight(evH + 40);
         rectangle.setWidth(6);
         rectangle.setFill(Paint.valueOf("#ff0000"));
-        int len = strings[0].length()-strings[1].length();
-        if(len<=0) {
+        int len = strings[0].length() - strings[1].length();
+        if (len <= 0) {
             rectangle.setX(0);
-        }else {
-            rectangle.setX(len*36);
+        } else {
+            rectangle.setX(len * 36);
         }
 
         rectangle.setY(48);
         return rectangle;
     }
-    private Label getEngText(){
+
+    private Label getEngText() {
         Label label = new Label(strings[4].toUpperCase());
         setTextFont(label);
         label.setWrapText(true);
         label.setAlignment(Pos.TOP_LEFT);
-        label.setPrefWidth(strings[1].length()*36+strings[3].length()*36+strings[2].length()*84);//设置固定宽度，以控制自动换行
-        int len = strings[0].length()-strings[1].length();
-        if(len<=0) {
+        label.setPrefWidth(strings[1].length() * 36 + strings[3].length() * 36 + strings[2].length() * 84);//设置固定宽度，以控制自动换行
+        int len = strings[0].length() - strings[1].length();
+        if (len <= 0) {
             label.setLayoutX(10);
-        }else {
-            label.setLayoutX(len*36+10);
+        } else {
+            label.setLayoutX(len * 36 + 10);
         }
         label.setLayoutY(96);
         System.out.println(label.getHeight());
